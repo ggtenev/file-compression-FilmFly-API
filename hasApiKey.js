2cd4b9169a9d15d2295f91c4905900ac5f41104d
HussnainQuresshi <hussnain77133@gmail.com>
2022-02-18 23:47:33 +0500

    update

--- hasApiKey.js
+++ hasApiKey.js
@@ -2,9 +2,9 @@
 
 module.exports = (req, res, next) => {
   try {
-    let { authorization } = req.headers;
+    let { authorization = "" } = req.headers;
+    console.log({ authorization, x: req.headers });
     authorization = authorization.split(" ")[1];
-    console.log({ authorization });
     if (!authorization) {
       return res.status(401).send("Unauthorized");
     }
