5030260ac00faaaeb9f6b239095324109cbffea9
HussnainQuresshi <hussnain77133@gmail.com>
2022-02-19 03:23:33 +0500

    update

--- middleware/createZip.js
+++ middleware/createZip.js
@@ -5,12 +5,8 @@ module.exports = async (req, res, next) => {
   try {
     const zip = new JSZip();
     const files = req.files;
-    const folder = path.join(__dirname, "../uploads", req.body.user_id);
-    fs.mkdirSync(folder, { recursive: true });
     files.forEach((file) => {
       file.buffer = fs.readFileSync(file.path);
-      const filePath = path.join(folder, file.filename);
-      fs.writeFileSync(filePath, file.buffer);
       zip.file(file.filename, file.buffer);
     });
     const zipFile = await zip.generateAsync({
@@ -21,10 +17,9 @@ module.exports = async (req, res, next) => {
       },
     });
     req.body.base64 = zipFile;
-    fs.readdirSync(folder).forEach((file) => {
-      fs.unlinkSync(path.join(folder, file));
+    fs.readdirSync(path.join(__dirname, "../uploads")).forEach((file) => {
+      fs.unlinkSync(path.join(__dirname, "../uploads", file));
     });
-    fs.rmdirSync(folder);
     next();
   } catch (ex) {
     return res.status(400).json({
