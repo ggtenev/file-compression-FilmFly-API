94ecf6f6df71f81ca32fc4402a176e4d87deb888
ggtenev <georgiy.tenev@gmail.com>
2022-02-18 02:57:03 +0000

    18/02/22 before deploy to ec2

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const createZip = async (req, res, next) => {
       zip
         .generateNodeStream({ type: "nodebuffer", streamFiles: true })
         .pipe(fs.createWriteStream(`${req.params.id}.zip`))
-        .on("finish", function () {
+        .on("finish", async function () {
           for (const file of files) {
             fs.unlink(`${req.params.id}/${file}`, (err) => {
               if (err) {
@@ -70,13 +70,44 @@ const createZip = async (req, res, next) => {
           fs.rmdir(__dirname + `/${req.params.id}`, () => {
             console.log("Folder Deleted!");
           });
+
+          try {
+            upload_to_s3({
+              fileName: `${req.params.id + Math.random(5).toString()}.zip`,
+              fileLocation: `${__dirname}/${req.params.id}.zip`,
+            }).then((url) => {
+              console.log(url);
+              fs.unlinkSync(`${__dirname}/${req.params.id}.zip`);
+              res.status(200).json({ url });
+            });
+
+            // fs.unlink(`${__dirname}/${req.params.id}.zip`, (err) => {
+            //   if (err) console.log(err);
+            //   else {
+            //     console.log("\nDeleted file: example_file.txt");
+
+            //   }
+            // });
+            // return res.status(200).send(
+            //   template({
+            //     link: url,
+            //     image:
+            //       "data:image/jpeg;base64," +
+            //       fs.readFileSync("./backgroun-image.jpeg", "base64"),
+            //     fileName: `${req.params.id}.zip`,
+            //   })
+            // );
+          } catch (ex) {
+            return res.status(400).json({ message: ex.message });
+          }
         });
     });
   } catch (err) {
     console.error(err);
+    return res.status(400).json({ message: err.message });
   }
 
-  next();
+  // next();
 };
 
 // Storage Engin That Tells/Configures Multer for where (destination) and how (filename) to save/upload our files
@@ -114,29 +145,7 @@ app.post(
   "/multiple/:id",
   createUserDirectory,
   upload.array("images"),
-  createZip,
-  async (req, res) => {
-    try {
-      let url = await upload_to_s3({
-        fileName: `${req.params.id}.zip`,
-        fileLocation: `${__dirname}/${req.params.id}.zip`,
-      });
-      fs.unlinkSync(`${__dirname}/${req.params.id}.zip`);
-      return res.status(200).send(
-        template({
-          link: url,
-          image:
-            "data:image/jpeg;base64," +
-            fs.readFileSync("./backgroun-image.jpeg", "base64"),
-          fileName: `${req.params.id}.zip`,
-        })
-      );
-    } catch (ex) {
-      return res.status(400).json({ message: ex.message });
-    }
-
-    // res.send("Multiple Files Upload Success");
-  }
+  createZip
 );
 app.listen(process.env.PORT || 3000);
 //cron job to delete the zip file after 7 day from s3
--- utils.js
+++ utils.js
@@ -1,25 +1,60 @@
+// const S3 = require("aws-sdk/client/s3");
+const fs = require("fs");
+
+// const region = process.env.AWS_REGION;
+// const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+// const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY_ID;
+// const bucketName = process.env.AWS_BUCKET_NAME;
+
+// const s3 = new S3({
+//   region,
+//   accessKeyId,
+//   secretAccessKey,
+//   // bucketName,
+// });
+
 /**
  * @description: This function is used to upload [image,pdf,video,audio, file.* ] to the s3 bucket
  * @body { file: base64 string, fileName:string, fileType:string, fileExtension:string (.ext), filePath:string(folder name on s3 on which you want file to be saved) }
  * @returns: {url} url of the uploaded file
  */
+
+// function upload(file) {
+//   const fileStream = fs.createReadStream(file.path);
+
+//   const uploadParams = {
+//     Bucket: bucketName,
+//     Body: fileStream,
+//     key: file.fileName,
+//   };
+
+//   return s3.uplaod(uploadParams).promise();
+// }
+
+// exports.uploadFile = upload;
+
 exports.upload_to_s3 = async ({ fileName, fileLocation }) => {
   try {
     const AWS = require("aws-sdk");
     AWS.config.update({
       region: process.env.AWS_REGION,
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_ACCESS_KEY_ID,
-      Bucket: "BUCKET_NAME",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
+      bucketName: process.env.AWS_BUCKET_NAME,
     });
     const s3 = new AWS.S3();
     let data = await s3
       .upload({
-        Bucket: "BUCKET_NAME",
+        Bucket: process.env.AWS_BUCKET_NAME,
         Key: fileName,
         Body: fs.readFileSync(fileLocation),
-        ACL: "public-read",
+        // ACL: "public-read",
       })
+      .on("httpUploadProgress", (evt) =>
+        console.log(
+          "Uploaded :: " + parseInt((evt.loaded * 100) / evt.total) + "%"
+        )
+      )
       .promise();
     return data.Location;
   } catch (ex) {
