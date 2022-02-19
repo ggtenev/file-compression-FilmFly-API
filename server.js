5701363ec578ace8b634184be62997714f444e50
HussnainQuresshi <hussnain77133@gmail.com>
2022-02-19 20:52:40 +0500

    update

--- server.js
+++ server.js
@@ -34,7 +34,11 @@ server.post("/v1/upload", hasApiKey, upload_to_s3);
  *  uploaded files creates a zip from them , You need to send files as a
  *  formdata along with user_id and api_key in the authroization header.
  */
-server.post("/v2/upload", [upload.array(), createZip], upload_to_s3_v2);
+server.post(
+  "/v2/upload",
+  [hasApiKey, upload.array(), createZip],
+  upload_to_s3_v2
+);
 
 server.get("/health", (req, res) => {
   res.status(200).send("ok");
